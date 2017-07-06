101cf707a57cb8d2d364877953bc9b11d268acd4
bogorodvo <bogorodz@gmail.com>
2017-07-07 01:24:38 +0300

    version 3

---
+++ test_task_0/script2.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+import csv
+import requests
+import sys
+
+with open(sys.argv[1]) as inf, open('wikipedia_answers.csv', 'w') as ouf:
+    reader = csv.reader(inf, delimiter="\t")
+    writer = csv.writer(ouf, quoting=csv.QUOTE_ALL)
+    writer.writerow(["wikipedia_page","website"])
+    for row in reader:
+        for line in requests.get(row[0]).text.split('</'):
+            if 'rel="nofollow" class="external' in line and 'th>\n<td' in line:
+                line = line[line.find('href="')+len('href="'):]
+                writer.writerow([row[0],line[:line.find('"')]])
+                break
