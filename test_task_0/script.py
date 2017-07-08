24a3633e0e1c127332fdde773638cf4cd6729718
Vladislav <bogorodz@gmail.com>
2017-07-08 13:07:50 +0300

    version 3

--- test_task_0/script.py
+++ test_task_0/script.py
@@ -9,11 +9,7 @@ with open(sys.argv[1]) as inf, open('wikipedia_answers.csv', 'w') as ouf:
     writer.writerow(["wikipedia_page","website"])
     for row in reader:
         for line in requests.get(row[0]).text.split('</'):
-            if 'rel="nofollow" class="external' in line:
-                if 'th>\n<td' in line:
-                    if 'http' in line:
-                        line = line[line.find('http'):]
-                    else:
-                        line = line[line.find('www'):]
-                    writer.writerow([row[0],line[:line.find('"')]])
-                    break
+            if 'a rel="nofollow" class="external' in line and 'th>\n<td' in line:
+                line = line[line.find('href="')+len('href="'):]
+                writer.writerow([row[0],line[:line.find('"')]])
+                break
