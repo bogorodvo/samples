6385ab37e7d3f682ebe75f75f3268d2455039d44
Vladislav <bogorodz@gmail.com>
2017-07-08 13:31:23 +0300

    version 3

--- test_task_0/script.py
+++ test_task_0/script.py
@@ -6,10 +6,10 @@ import sys
 with open(sys.argv[1]) as inf, open('wikipedia_answers.csv', 'w') as ouf:
     reader = csv.reader(inf, delimiter="\t")
     writer = csv.writer(ouf, quoting=csv.QUOTE_ALL)
-    writer.writerow(["wikipedia_page","website"])
+    writer.writerow(["wikipedia_page", "website"])
     for row in reader:
         for line in requests.get(row[0]).text.split('</'):
             if 'a rel="nofollow" class="external' in line and 'th>\n<td' in line:
-                line = line[line.find('href="')+len('href="'):]
-                writer.writerow([row[0],line[:line.find('"')]])
+                line = line[line.find('href="') + len('href="'):]
+                writer.writerow([row[0], line[:line.find('"')]])
                 break
