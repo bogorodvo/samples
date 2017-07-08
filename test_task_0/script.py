5c4e944c4516799c962959bedd52b56c02bb3894
Vladislav <bogorodz@gmail.com>
2017-07-08 16:53:10 +0300

    version 4

--- test_task_0/script.py
+++ test_task_0/script.py
@@ -1,15 +1,32 @@
+
 #!/usr/bin/python3
 import csv
 import requests
 import sys
 
-with open(sys.argv[1]) as inf, open('wikipedia_answers.csv', 'w') as ouf:
-    reader = csv.reader(inf)
-    writer = csv.writer(ouf, quoting=csv.QUOTE_ALL)
-    writer.writerow(["wikipedia_page", "website"])
-    for row in reader:
-        for line in requests.get(row[0]).text.split('</'):
-            if 'rel="nofollow" class="external' in line and 'th>\n<td' in line:
-                line = line[line.find('href="')+len('href="'):]
-                writer.writerow([row[0], line[:line.find('"')]])
-                break
+
+class Task:
+    def __init__(self):
+        self.file_inf = sys.argv[1]
+        self.file_ouf = 'wikipedia_answers.csv'
+        self.head_ouf = ["wikipedia_page", "website"]
+        self.s_split = '</'
+        self.s_compare = ['rel="nofollow" class="external', 'th>\n<td']
+        self.s_start = 'href="'
+        self.s_end = '"'
+
+    def content(self, link):
+        for line in requests.get(link[0]).text.split(self.s_split):
+            if self.s_compare[0] in line and self.s_compare[1] in line:
+                line = line[line.find(self.s_start) + len(self.s_start):]
+                return line[:line.find(self.s_end)]
+
+    def file_reading(self):
+        with open(self.file_inf, 'r') as inf, open(self.file_ouf, 'w') as ouf:
+            reader = csv.reader(inf, delimiter='\t')
+            writer = csv.writer(ouf, quoting=csv.QUOTE_ALL)
+            writer.writerow(self.head_ouf)
+            for row in reader:
+                writer.writerow([row[0], self.content(row)])
+
+Task().file_reading()
