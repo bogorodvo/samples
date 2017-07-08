d6a386682e0d4605698d02fa03302de081194916
Vladislav <bogorodz@gmail.com>
2017-07-08 16:29:00 +0300

    Class

---
+++ test_task_0/scrypt2.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+import csv
+import requests
+import sys
+
+
+class Task:
+    def __init__(self):
+        self.file_inf = 'wikipedia_links.csv'
+        self.file_ouf = 'wikipedia_answers.csv'
+        self.head_ouf = ["wikipedia_page", "website"]
+        self.s_split = '</'
+        self.s_compare = ['rel="nofollow" class="external', 'th>\n<td']
+        self.s_start = 'href="'
+        self.s_end = '"'
+
+    def content(self, link):
+        for line in requests.get(link[0]).text.split(self.s_split):
+            if self.s_compare[0] in line and self.s_compare[1] in line:
+                line = line[line.find(self.s_start) + len(self.s_start):]
+                return line[:line.find(self.s_end)]
+
+    def file_readings(self):
+        with open(self.file_inf, 'r') as inf, open(self.file_ouf, 'w') as ouf:
+            reader = csv.reader(inf)
+            writer = csv.writer(ouf, quoting=csv.QUOTE_ALL)
+            writer.writerow(self.head_ouf)
+            for row in reader:
+                writer.writerow([row[0], self.content(row)])
+
+Task().file_readings()
