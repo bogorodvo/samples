867697704430e14545ed74f5e2c1e0aed1eef898
Vladislav <bogorodz@gmail.com>
2017-07-08 17:57:12 +0300

    version 5

--- test_task_0/script.py
+++ test_task_0/script.py
@@ -9,16 +9,15 @@ class Task:
         self.file_inf = sys.argv[1]
         self.file_ouf = 'wikipedia_answers.csv'
         self.head_ouf = ["wikipedia_page", "website"]
-        self.s_split = '</'
-        self.s_compare = ['rel="nofollow" class="external', 'th>\n<td']
+        self.s_compare = '>Website<'
         self.s_start = 'href="'
         self.s_end = '"'
 
     def content(self, link):
-        for line in requests.get(link[0]).text.split(self.s_split):
-            if self.s_compare[0] in line and self.s_compare[1] in line:
-                line = line[line.find(self.s_start) + len(self.s_start):]
-                return line[:line.find(self.s_end)]
+        line = requests.get(link[0]).text
+        line = line[line.find(self.s_compare):]
+        line = line[line.find(self.s_start) + len(self.s_start):]
+        return line[:line.find(self.s_end)]
 
     def file_reading(self):
         with open(self.file_inf, 'r') as inf, open(self.file_ouf, 'w') as ouf:
